92a8a315731c5bf95c0721952afe1305f40c4db5
Sean Houde <seanhoude@gmail.com>
2018-12-06 20:59:12 -0500

    get json data from itunes api

--- main.js
+++ main.js
@@ -1,24 +1,46 @@
 const search = $('#searchbar')
 search.val('emancipator')
 
+const player = $('#player')
 const searchButton = $('#search-button')
 
 let $songsDiv = $('#songlist')
                     
 $('#search-button').on('click', function(evt) {
-
     let term = search.val()
 
-    $.get('https://itunes-api-proxy.glitch.me/',
+    $.get('https://itunes.apple.com/search?term=',
         {
             term: term,
-            media: 'music'
+            media: 'music',
+            limit: 30
         },
         function(data) {
             console.log(data)
+            console.log(data.resultCount)
+            $songsDiv.empty()
+            
+            $.each(
+                data.results,
+                function(result, $songsDiv) {
+                    console.log(result.artistName)
+                    console.log(result.artistViewUrl)
+                    console.log(result.artworkUrl100)
+                    console.log(result.collectionName)
+                    console.log(result.collectionViewUrl)
+                    console.log(result.trackName)
+                    console.log(result.previewUrl)
+                    $songsDiv.append(`<p>${result.artistName}</p>`)
+                    player.attr({src: result.previewUrl})
+
+        
+                },
+            )
         },
+        'json'
     )
 })
+
     // function getSongs(term) {
     //     $.ajax({   
     //         url: 'https://itunes.apple.com/',
