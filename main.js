2825151fd6580f1e8fac3f563dd23dfb459ae92e
Sean Houde <SeanHoude@Gmail.com>
2018-12-07 00:21:26 -0500

    fix layout, add play image overlay, add players for each card

--- main.js
+++ main.js
@@ -2,65 +2,52 @@ const search = $('#searchbar')
 search.val('emancipator')
 
 const player = $('#player')
-const searchButton = $('#search-button')
-
-let $songsDiv = $('#songlist')
-                    
-$('#search-button').on('click', function(evt) {
-    let term = search.val()
-
+const $searchButton = $('#search-button')
+let term = search.val()
+     
+function retrieve(term) {
     $.get('https://itunes.apple.com/search?term=',
         {
-            term: term,
+            term: search.val(),
             media: 'music',
             limit: 30
         },
         function(data) {
-            console.log(data)
-            console.log(data.resultCount)
-            $songsDiv.empty()
+            // console.log(data.results)
+            $('#songlist').empty()
             
-            $.each(
-                data.results,
-                function(result, $songsDiv) {
-                    console.log(result.artistName)
-                    console.log(result.artistViewUrl)
-                    console.log(result.artworkUrl100)
-                    console.log(result.collectionName)
-                    console.log(result.collectionViewUrl)
-                    console.log(result.trackName)
-                    console.log(result.previewUrl)
-                    $songsDiv.append(`<p>${result.artistName}</p>`)
-                    player.attr({src: result.previewUrl})
-
-        
-                },
-            )
+            $.each(data.results, function(i, result) {
+                if (i > 10) {return false}
+                let albumArt = result.artworkUrl100.replace('100x100','550x550')
+                let cardHtml = `
+                    <div class="itemsContainer">
+                        <div><img class="album-art thumbnail" src="${albumArt}">
+                            <div class="play"><img src="play.png" />
+                            </div>
+                        </div>
+                    </div>
+                    <h5>${result.trackName}</h5>
+                    <p><a href="${result.collectionViewUrl}" >${result.collectionName} - </a><a href="${result.artistViewUrl}">${result.artistName}</a></p>
+                    <audio class="players" controls src="${result.previewUrl}">
+                        Your browser does not support the <code>audio</code> element.
+                    </audio>
+                ` 
+                $('#songlist').append($(`<div class="column song" data="${result.previewUrl}" />`).html(cardHtml))
+                $('#player').attr({src: result.previewUrl})
+            })
         },
         'json'
     )
+}
+
+$('#search-button').on('click', function(evt) {
+    retrieve()
 })
 
-    // function getSongs(term) {
-    //     $.ajax({   
-    //         url: 'https://itunes.apple.com/',
-    //         data: {
-    //             term: term,
-    //             media: 'music',
-    //         },
-    //         crossDomain: true,
-    //         dataType: 'jsonp',
-    //         method: 'GET',
-    //         success: function (data) {
-    //             console.log(data)
-    //             console.log('success!!!')
-    //             // $songsDiv.empty()
-    //             // $.each(data.)
-    //         },
-    //         error: function(error) {
-    //             console.log(error)
-    //             console.log('error!!!')
-    //         }
-    //     })
-    // }
-    // getSongs(term)
+$('#search-button').on('blur keydown', function() {
+    if ( !event.keyCode || event.keyCode == 13 ) {
+      retrieve();
+    }
+});
+  
+retrieve();
