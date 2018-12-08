acef786149b849c3cb734edb255b6b8ecb25323e
Sean Houde <seanhoude@gmail.com>
2018-12-08 10:02:39 -0500

    remove individual music players, add autoplay song on album art click to top player

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const player = $('#player')
 const $searchButton = $('#search-button')
 let term = search.val()
      
-function retrieve(term) {
+function retrieve() {
     $.get('https://itunes.apple.com/search?term=',
         {
             term: search.val(),
@@ -13,41 +13,53 @@ function retrieve(term) {
             limit: 30
         },
         function(data) {
-            // console.log(data.results)
+            console.log(data.results)
             $('#songlist').empty()
             
             $.each(data.results, function(i, result) {
-                if (i > 10) {return false}
+                if (i > 29) {return false}
                 let albumArt = result.artworkUrl100.replace('100x100','550x550')
                 let cardHtml = `
                     <div class="itemsContainer">
-                        <div><img class="album-art thumbnail" src="${albumArt}">
-                            <div class="play"><img src="play.png" />
+                        <div><img data-url="${result.previewUrl}" data-track="${result.trackName} - ${result.artistName}" class="album-art thumbnail" src="${albumArt}">
+                            <div class="play"><img data-url="${result.previewUrl}" data-track="${result.trackName} - ${result.artistName}" class="album-art" src="play.png" />
                             </div>
                         </div>
                     </div>
                     <h5>${result.trackName}</h5>
                     <p><a href="${result.collectionViewUrl}" >${result.collectionName} - </a><a href="${result.artistViewUrl}">${result.artistName}</a></p>
-                    <audio class="players" controls src="${result.previewUrl}">
-                        Your browser does not support the <code>audio</code> element.
-                    </audio>
                 ` 
                 $('#songlist').append($(`<div class="column song" data="${result.previewUrl}" />`).html(cardHtml))
+                let $title = `<p>${result.trackName} - ${result.artistName}</p>`
                 $('#player').attr({src: result.previewUrl})
+                $('#player-container').empty().append($title)
             })
         },
         'json'
     )
 }
 
-$('#search-button').on('click', function(evt) {
+$('#search-button').on('click', function() {
     retrieve()
 })
 
-$('#search-button').on('blur keydown', function() {
-    if ( !event.keyCode || event.keyCode == 13 ) {
-      retrieve();
+$(document).on('blur keyup', function(evt) {
+    console.log(evt)
+    if ( !evt.keyCode || evt.keyCode == 13 ) {
+      retrieve()
     }
-});
-  
+})
+
+$('#songlist').on('click', function(evt) {
+    if (evt.target.classList.contains('album-art')) {
+    let player = `
+        <audio id="player" autoplay controls src="${evt.target.dataset['url']}">
+        Your browser does not support the <code>audio</code> element.
+        </audio>
+        <p>${evt.target.dataset['track']}</p>
+    `
+    console.log(evt.target.dataset['url'])
+    $('#player-container').empty().append(player)
+}})
+
 retrieve();
